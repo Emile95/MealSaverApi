8855b643b6ec961f138d21d78996696e34fc654e
Emile95 <desjardins.e@outlook.com>
2020-05-19 17:00:02 -0400

    Add data validation for Aliment

--- Application/Instances/Aliment/DataModel/Sended/Aliment/Validation.cs
+++ Application/Instances/Aliment/DataModel/Sended/Aliment/Validation.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Interface.SendedDataValidation;
 using DataValidator.Configuration;
 using Persistance.RepositoryProfiles;
+using Persistance.RepositoryProfiles.Aliment;
 using RepositoryManager;
 using System;
 using System.Globalization;
@@ -25,6 +26,15 @@ namespace Application.Aliment.DataModel.Sended
 
         #region Private Methods
 
+        public bool CheckIfNonExistant(int accountId, string Name)
+        {
+            return _repositoryManager
+                .Repository<Persistance.Entities.Aliment>()
+                .Select<AlimentInfoSelector>(o => o.AccountId == accountId)
+                .Where(o => string.Equals(o.Name, Name, StringComparison.OrdinalIgnoreCase))
+                .Count() == 0;
+        }
+
         #endregion
 
         #region ISendedDataValidation Implementation
@@ -39,7 +49,16 @@ namespace Application.Aliment.DataModel.Sended
                         .ForNoValidate(o => o == null)
                         .ForValidate(
                             o => o.Length <= 20,
-                            "MaxCharacters:20"
+                            "MaxCharacters=20"
+                        )
+                )
+                .ForValue(
+                    "Name",
+                    data => new Tuple<string,int>(data.Name, data.AccountId.Value),
+                    value => value
+                        .ForValidate(
+                            o => CheckIfNonExistant(o.Item2, o.Item1),
+                            "ExistAlready"
                         )
                 );
         }
